ceb27f76c758b4b561fb7b200b8c3593d6fa990a
tk_thyme <tk.thyme@outlook.com>
2023-05-02 17:39:44 +0800

    Cleaning more data and turn the ts in to long form

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 # main
 if __name__ == '__main__':
+    os.chdir(os.path.join('.\ECON4999X'))
     RAW_DATA_DIR = os.path.normpath(r'.\Data')
     print(os.listdir(RAW_DATA_DIR))
 
@@ -91,4 +92,43 @@ if __name__ == '__main__':
     
     cleaned_file_name = 'df_ghg_ts_master.csv'
     df_ghg_ts_master.to_csv(os.path.join('.', 'Cleaned_Data', cleaned_file_name), index=True)
+
+    # Turn the time series into long form
+    # Load the dataframe
+    CLEANED_DATA_DIR = os.path.join(r'.\Cleaned_data')
+    ts_file_name = 'df_power_ts_master.csv'
+    ts_file_path = os.path.join(CLEANED_DATA_DIR, ts_file_name)
+    df = pd.read_csv(ts_file_path)
+
+    # melt the df
+    df_long_form = df.melt(id_vars='Entity ID')
+    pattern_year = r'_CY(\d{4})'
+    pattern_var = r'TC_COMPANY_POWER_GEN_(.+)_CY.*'
+    df_long_form['year'] = df_long_form['variable'].str.extract(pattern_year)
+    df_long_form['power'] = df_long_form['variable'].str.extract(pattern_var)
+    df_long_form.head()
+
+    df_long_form = df_long_form[['Entity ID', 'power', 'year', 'value']]
+
+    cleaned_file_name = 'df_power_ts_long_form.csv'
+    df_long_form.to_csv(os.path.join('.', 'Cleaned_Data', cleaned_file_name), index=False)
+
+
+if __name__ == '__utils__':
+    CLEANED_DATA_DIR = os.path.join(r'.\Cleaned_data')
+    file_name = 'df_info_master.csv'
+    file_path = os.path.join(CLEANED_DATA_DIR, file_name)
+    df_company_info_master = pd.read_csv(file_path)
+    df_company_info_master.columns
+    # Print industry list
+    industry_list = df_company_info_master['Industry Classification'].unique().tolist()
+    _ = [print(x) for x in industry_list]
+
+    # Print primary industry list
+    pri_industry_list = df_company_info_master['Primary Industry'].unique().tolist()
+    _ = [print(x) for x in pri_industry_list]
+
+    # Print secondary industry list
+    sec_industry_list = df_company_info_master['2nd Level Primary Industry'].unique().tolist()
+    _ = [print(x) for x in sec_industry_list]
 
